71aa367f196d10ee8fea1b93a421b115f63bde88
mrtanke <mrtanke2024@gmail.com>
2025-10-04 20:06:55 +0200

    chore: initial commit

---
+++ api/langchain_utils.py
@@ -0,0 +1,49 @@
+# Encapsulates the LangChain-specific logic, 
+# such as creating the RAG chain and configuring the language model.
+from langchain_openai import ChatOpenAI
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain.chains import create_history_aware_retriever, create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from typing import List
+from langchain_core.documents import Document
+import os
+from chroma_utils import vectorstore
+
+# Convert the vector store to the retriever
+retriever = vectorstore.as_retriever(search_kwargs={"k": 2})
+
+output_parser = StrOutputParser()
+
+# Set up prompts
+contextualize_q_system_prompt = (
+    "Given a chat history and the latest user question "
+    "which might reference context in the chat history, "
+    "formulate a standalone question which can be understood "
+    "without the chat history. Do NOT answer the question, "
+    "just reformulate it if needed and otherwise return it as is."
+)
+
+# reformulate the user's question based on chat history.
+contextualize_q_prompt = ChatPromptTemplate.from_messages([
+    ("system", contextualize_q_system_prompt),
+    MessagesPlaceholder("chat_history"),
+    ("human", "{input}"),
+])
+
+# generate the final answer based on the retrieved context and chat history.
+qa_prompt = ChatPromptTemplate.from_messages([
+    ("system", "You are a helpful AI assistant. Use the following context to answer the user's question."),
+    ("system", "Context: {context}"),
+    MessagesPlaceholder("chat_history"),
+    ("human", "{input}")
+])
+
+
+# Create the RAG chain
+def get_rag_chain(model="gpt-4o-mini"):
+    llm = ChatOpenAI(model=model)
+    history_aware_retriever = create_history_aware_retriever(llm, retriever, contextualize_q_prompt)
+    question_answer_chain = create_stuff_documents_chain(llm, qa_prompt)
+    rag_chain = create_retrieval_chain(history_aware_retriever, question_answer_chain)    
+    return rag_chain
